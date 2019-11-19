1072b3732a448e03b8740608740407df8bc9f068
OlivierArgentieri <olivierargentieri@gmail.com>
2019-11-19 09:10:17 +0100

    test integration

---
+++ InputManager/Assets/InputManager/Import/SingleTemplate/SingletonTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonTemplate<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance = default(T);
+    public static T Instance => instance;
+
+
+    private void Awake()
+    {
+        InitSingleton();
+    }
+
+    private void InitSingleton()
+    {
+        if (instance == null)
+            instance = this as T;
+
+        if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        
+        
+    }
+}
---
+++ InputManager/Assets/InputManager/Scripts/InputManager/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : SingletonTemplate<InputManager>
+{
+    [SerializeField] public List<MyKey> ActionKey;
+
+
+    private void Update()
+    {
+        foreach (var myKey in ActionKey)
+        {
+            if(Input.GetKeyDown(myKey.Key))
+                Debug.Log("test");
+        }
+    }
+}
